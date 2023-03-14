0a8c0022aca3bec317906cd7b3c5368c70671c39
Paul Walker <paulwalker@paulwalker.dev>
2023-03-14 08:40:22 -0400

    SSH Mistake

--- users/default.nix
+++ users/default.nix
@@ -9,7 +9,7 @@ in {
     admin = true;
     backup = true;
     ssh = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDGhg55ucXI1rypBjMQBZXsqK2G6OP8sDt9NpAwx+Qms"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK1ZKaSnmZNZmvf9kmKaO/6SyaBV2gC11/Cre9RUDnER"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF7DXKJns7+SW4Vff4R8TofQP2ReyyHy9iVH+ybFgE24"
     ];
   };
